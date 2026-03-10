d83057b1494bba3708c5d4101457cba656e91351
ana-portela <anabeatrizportela897@gmail.com>
2026-03-10 17:33:43 -0300

    Add files via upload

---
+++ aula03-revisao/recebe_dados.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RECEBE DADOS</title>
+</head>
+<body>
+    
+    <?php
+        //$nome = $_POST['nome'];
+        $nome = htmlspecialchars($_POST['nome']);
+        $email = $_POST['email'];
+        $idade = $_POST['idade'];
+
+        $ano_atual = date('Y');
+    
+      
+    ?>
+
+    <p>O nome é: <?php  echo $nome;  ?> </p>
+    <p>O email é: <?php  echo $email;  ?> </p>
+    <p>A idade é: <?php  echo $idade;  ?> </p>
+
+    <p> ahh então você nasceu no ano de: <?php  echo $ano_atual - $idade  ?> </p>
+
+    <?php
+    if ($idade >= 18) {
+        echo '<p style ="color: green"> Você é maior de idade </p>';
+    }else{
+        echo '<p style ="color: red"> Você é menor de idade </p>';
+    }
+    
+    ?>
+</body>
+</html>
